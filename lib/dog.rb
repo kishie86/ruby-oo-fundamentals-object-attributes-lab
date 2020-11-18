4809171807678d8d902483c025a93de1a34ffcaf
Kishana Liburd <kishana.liburd@gmail.com>
2020-11-17 20:20:15 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,30 @@
+require 'pry'
 class Dog
-    def initialize(name, breed)
+    def instantiate(name, breed)
         @name = name
         @breed = breed
     end
+
+def name
+    @name
 end
+
+def breed
+    @breed
+end
+
+def name=(new_name)
+    @name = new_name
+end
+
+def breed=(new_breed)
+    @breed = new_breed
+end
+
+end
+
+#snoopy = Dog.new("snoopy")
+
+#def dog_names
+#name = dog
+#end
