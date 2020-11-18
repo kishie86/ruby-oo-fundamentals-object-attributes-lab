d924c71d96a0569835f1bc08fbe44a3e8b6e2f68
Kishana Liburd <kishana.liburd@gmail.com>
2020-11-17 20:26:04 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,28 +1,23 @@
 require 'pry'
 class Person
-    def instantiate(name)
+    def instantiate(name, job)
         @name = name
+        @job = job
     end
 
  def name
     @name
 end
 
-
 def name=(new_name)
      @name = new_name
 end
-
-def instantiate(job)
-    @name = name
-end
-
 
 def job
     @job
 end
 
-def job(new_job)
+def job=(new_job)
     @job = new_job
 end
 
